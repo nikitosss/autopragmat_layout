88d578951c3b30fb4355ff7449c2c4bea79a0516
Nikita <kuznetsov.n.r@gmail.com>
2022-06-20 03:37:15 +0400

    upgrade layout

--- js/common.js
+++ js/common.js
@@ -1,31 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
   ymaps.ready(init);
 
   function init() {
-    var myMap = new ymaps.Map("footer_map", {
-        center: [55.751979, 37.617499],
-        zoom: 15
-      }),
+    var coord = [55.81242804203617, 37.631908033779695];
+    var footerMap = new ymaps.Map('footer_map', {
+      center: coord,
+      zoom: 16,
+    });
+    var placemark = new ymaps.Placemark(coord, null, {
+      iconImageHref: '../img/map_point.svg',
+      iconImageSize: [60, 68],
+      iconImageOffset: [-30, -68],
+    })
 
-      // Создаем метку с помощью вспомогательного класса.
-      myPlacemark1 = new ymaps.Placemark([55.751979, 37.617499], {
-        // Свойства.
-        // Содержимое хинта.
-        hintContent: 'Надпись, которая всплаывет при наведении на метку'
-      }, {
-        // Опции
-        // Своё изображение иконки метки.
-        iconImageHref: 'https://static.tildacdn.com/tild3061-3235-4537-b066-616662373363/Group_783.svg',
-        // Размеры метки.
-        iconImageSize: [130, 130],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-65, -110]
-      })
-
-
-    // Добавляем все метки на карту.
-    myMap.geoObjects
-      .add(myPlacemark1)
+    footerMap.geoObjects
+      .add(placemark)
   }
 });
